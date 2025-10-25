d8484fb41ee892574a77678d82b1485af299dca6
hakim319791979-del <hakim319791979@gmail.com>
2025-10-25 04:05:18 +0300

    Create country.dart

---
+++ lib/models/country.dart
@@ -0,0 +1,19 @@
+class Country {
+  final String code;
+  final String nameAr;
+  final String nameEn;
+  final String laws;
+  final String customs;
+  final String safety;
+  final String costs;
+
+  const Country({
+    required this.code,
+    required this.nameAr,
+    required this.nameEn,
+    required this.laws,
+    required this.customs,
+    required this.safety,
+    required this.costs,
+  });
+}
