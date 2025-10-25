5639c9582464dce4646903693b63e443feb5db92
hakim319791979-del <hakim319791979@gmail.com>
2025-10-25 08:41:49 +0300

    Update gradlew.bat

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,14 +1,3 @@
-@echo off
-@rem Gradle startup script for Windows
-
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-
-set DEFAULT_JVM_OPTS=
-
-set JAVA_EXE=java.exe
-if defined JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+@ECHO OFF
+set DIR=%~dp0
+"%DIR%gradle\wrapper\gradle-wrapper.jar" %*
